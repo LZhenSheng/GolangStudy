fd0237015a1e4273a07f0704e30f494d88335931
LZhenSheng <2759100807@qq.com>
2024-09-23 18:13:36 +0800

    update

--- Introduction/standard_streams.go
+++ Introduction/standard_streams.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
 type Student struct {
 	Name string
@@ -73,13 +78,18 @@ func test4() {
 	fmt.Printf("%e %e\n", num1, num2)
 	fmt.Printf("%E %E\n", num1, num2)
 	fmt.Printf("%f %f\n", num1, num2)
-	fmt.Printf("%F %F\n", num1, num2)
-	fmt.Printf("%g %g\n", num1, num2)
-	fmt.Printf("%e %e\n", num1, num2)
-	fmt.Printf("%G %G\n", num1, num2)
-	fmt.Printf("%E %E\n", num1, num2)
-	fmt.Printf("%x %x\n", num1, num2)
-	fmt.Printf("%X %X\n", num1, num2)
+	fmt.Printf("%.4f %.4f\n", num1, num2)
+	fmt.Printf("-%9f %9f-\n", num1, num2)
+	fmt.Printf("-%9.f %9.f-\n", num1, num2)
+	fmt.Printf("-%9.4f %9.4f-\n", num1, num2)
+
+	// fmt.Printf("%F %F\n", num1, num2)
+	// fmt.Printf("%g %g\n", num1, num2)
+	// fmt.Printf("%e %e\n", num1, num2)
+	// fmt.Printf("%G %G\n", num1, num2)
+	// fmt.Printf("%E %E\n", num1, num2)
+	// fmt.Printf("%x %x\n", num1, num2)
+	// fmt.Printf("%X %X\n", num1, num2)
 
 }
 func test5() {
@@ -89,12 +99,148 @@ func test5() {
 	fmt.Printf("%q %q\n", str1, str2)
 	fmt.Printf("%x %x\n", str1, str2)
 	fmt.Printf("%X %X\n", str1, str2)
+	fmt.Printf("%p %p\n", &str1, &str2)
+	fmt.Printf("%b %b\n", &str1, &str2)
+	fmt.Printf("%d %d\n", &str1, &str2)
+	fmt.Printf("%o %o\n", &str1, &str2)
+	fmt.Printf("%x %x\n", &str1, &str2)
+	fmt.Printf("%X %X\n", &str1, &str2)
 
+}
+func test6() {
+	fmt.Printf("%[2]d %[1]d\n", 11, 22)
+	fmt.Printf("%[3]*.[2]*[1]f\n", 12.0, 2, 6)
+	fmt.Printf("%6.2f\n", 12.0)
+}
+func test7() {
+	//类型错误或动词未知：%!verb(type=value)
+	fmt.Printf("%d", "hi")
+	fmt.Println()
+	//%!d(string=hi)
+
+	//参数太多：%!(EXTRA type=value)
+	fmt.Printf("hi", "guys")
+	fmt.Println()
+	//hi%!(EXTRA string=guys)
+
+	//参数太少：%!verb(MISSING)
+	fmt.Printf("hi%d\n")
+	fmt.Println()
+	//hi%!d(MISSING)
+
+	//宽度或精度非整数：%!(BADWIDTH) 或 %!(BADWIDTH)
+	fmt.Printf("%*s", 4.5, "hi")
+	fmt.Println()
+	//%!(BADWIDTH)hi
+	fmt.Printf("%.*s", 4.5, "hi")
+	fmt.Println()
+	//%!(BADPREC)hi
+
+	//参数索引无效或使用不正确：%!(BADINDEX)
+	fmt.Printf("%*[2]d", 7)
+	fmt.Println()
+	//%!d(BADINDEX)
+	fmt.Printf("%.[2]d", 7)
+	fmt.Println()
+	//%!d(BADINDEX)
+}
+func test8() {
+	const name, age = "Kim", 22
+	n, err := fmt.Fprint(os.Stdout, name, " is ", age, " years old.\n")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fprint: %v\n", err)
+	}
+	fmt.Print(n, " bytes written.\n")
+
+}
+func test9() {
+	var (
+		i int
+		b bool
+		s string
+	)
+	r := strings.NewReader("5 true gophers")
+	n, err := fmt.Fscanf(r, "%d %t %s", &i, &b, &s)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fscanf: %v\n", err)
+	}
+	fmt.Println(i, b, s)
+	fmt.Println(n)
+}
+func test10() {
+	const name, age = "Kim", 22
+	n, err := fmt.Fprintln(os.Stdout, name, "is", age, "years old.")
+
+	// The n and err return values from Fprintln are
+	// those returned by the underlying io.Writer.
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fprintln: %v\n", err)
+	}
+	fmt.Println(n, "bytes written.")
+
+}
+func test11() {
+	var (
+		i int
+		b bool
+		s string
+	)
+	r := strings.NewReader("5 true gophers")
+	n, err := fmt.Fscanf(r, "%d %t %s", &i, &b, &s)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fscanf: %v\n", err)
+	}
+	fmt.Println(i, b, s)
+	fmt.Println(n)
+}
+func test12() {
+	var (
+		i int
+		b bool
+		s string
+	)
+	r := strings.NewReader("5 true gophers")
+	n, err := fmt.Fscan(r, &i, &b, &s)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fscanf: %v\n", err)
+	}
+	fmt.Println(i, b, s)
+	fmt.Println(n)
+}
+func test13() {
+	s := `dmr 1771 1.61803398875
+	ken 271828 3.14159`
+	r := strings.NewReader(s)
+	var a string
+	var b int
+	var c float64
+	for {
+		n, err := fmt.Fscanln(r, &a, &b, &c)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%d: %s, %d, %f\n", n, a, b, c)
+	}
+}
+func test14() {
+	const name, age = "Kim", 22
+	fmt.Print(name, " is ", age, " years old.\n")
 }
 func main() {
 	//test1()
 	//test2()
 	// test3()
 	// test4()
-	test5()
+	// test5()
+	// test6()
+	//test7()
+	//test8()
+	// test9()
+	// test10()
+	//test12()
+	// test13()
+	test14()
 }
