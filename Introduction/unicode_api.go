708124c27db78bda4e6a71973196965de43a44ed
LZhenSheng <2759100807@qq.com>
2024-09-24 22:13:35 +0800

    update

--- Introduction/unicode_api.go
+++ Introduction/unicode_api.go
@@ -31,13 +31,36 @@ func unicode_api_test6() {
 	fmt.Printf("%t\n", unicode.IsTitle('ǅ'))
 	fmt.Printf("%t\n", unicode.IsTitle('a'))
 }
-func unicode_api_test() {
+func unicode_api_test7() {
 	fmt.Printf("%#U\n", unicode.SimpleFold('A'))      // 'a'
 	fmt.Printf("%#U\n", unicode.SimpleFold('a'))      // 'A'
 	fmt.Printf("%#U\n", unicode.SimpleFold('K'))      // 'k'
 	fmt.Printf("%#U\n", unicode.SimpleFold('k'))      // '\u212A' (Kelvin symbol, K)
 	fmt.Printf("%#U\n", unicode.SimpleFold('\u212A')) // 'K'
 	fmt.Printf("%#U\n", unicode.SimpleFold('1'))      // '1'
+}
+func unicode_api_test8() {
+	const lcG = 'g'
+	fmt.Printf("%#U\n", unicode.To(unicode.UpperCase, lcG))
+	fmt.Printf("%#U\n", unicode.To(unicode.LowerCase, lcG))
+	fmt.Printf("%#U\n", unicode.To(unicode.TitleCase, lcG))
+
+	const ucG = 'G'
+	fmt.Printf("%#U\n", unicode.To(unicode.UpperCase, ucG))
+	fmt.Printf("%#U\n", unicode.To(unicode.LowerCase, ucG))
+	fmt.Printf("%#U\n", unicode.To(unicode.TitleCase, ucG))
+}
+func unicode_api_test9() {
+	const ucG = 'G'
+	fmt.Printf("%#U\n", unicode.ToLower(ucG))
+}
+func unicode_api_test10() {
+	const ucG = 'g'
+	fmt.Printf("%#U\n", unicode.ToTitle(ucG))
+}
+func unicode_api_test() {
+	const ucG = 'g'
+	fmt.Printf("%#U\n", unicode.ToUpper(ucG))
 }
 func main() {
 	unicode_api_test()
