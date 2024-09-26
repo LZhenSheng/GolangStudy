97aa0778f2661167b52afb61a3a7f098018b706d
LZhenSheng <2759100807@qq.com>
2024-09-26 12:08:42 +0800

    update

--- Introduction/type_using.go
+++ Introduction/type_using.go
@@ -10,6 +10,28 @@ type MyInt2 int
 func (mi MyInt2) string() string {
 	return strconv.Itoa(int(mi))
 }
+
+type MyPerson struct {
+	name    string
+	age     int
+	address string
+	height  float32
+}
+
+//	type MyStudent struct {
+//		p     MyPerson
+//		score float32
+//	}
+
+//	type MyStudent struct {
+//		address struct {
+//			city string
+//		}
+//		score float32
+//	}
+func (p MyPerson) print() {
+	fmt.Printf("name:%s,age:%d\n", p.name, p.age)
+}
 func main() {
 	//type 1定义结构体2定义类型别名3定义接口4类型定义
 	//别名是为了更好的理解代码
@@ -32,4 +54,57 @@ func main() {
 	}
 	m := a.(string)
 	fmt.Println(m)
+
+	p1 := MyPerson{
+		"boddy1",
+		18,
+		"慕课网",
+		1.80,
+	}
+	p2 := MyPerson{
+		name: "boddy1",
+		age:  18,
+	}
+	fmt.Println(p1)
+	fmt.Println(p2)
+	person2 := []MyPerson{
+		{
+			name: "22323",
+		},
+		{
+			name: "34",
+		},
+	}
+	fmt.Println(person2)
+
+	var p MyPerson
+	p.age = 20
+	fmt.Println(p.height)
+	//匿名结构体
+	addres := struct {
+		province string
+		city     string
+		address  string
+	}{
+		province: "北京市",
+		city:     "通州区",
+		address:  "xxx",
+	}
+	fmt.Println(addres)
+
+	// s := MyStudent{
+	// 	MyPerson{
+	// 		name: "boddy1",
+	// 		age:  18,
+	// 	},
+	// 	19,
+	// }
+	// fmt.Println(s.p.age)
+
+	// ss := MyStudent{}
+	// ss.p.name = "dlkfj"
+	// ss := MyStudent{}
+	// ss.address.city = "sdkjflkj"
+	// fmt.Println(ss)
+	p2.print()
 }
