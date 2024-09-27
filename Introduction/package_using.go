a46911267c9af5c1864b0a6af8257ce5cce5866d
LZhenSheng <2759100807@qq.com>
2024-09-27 16:12:47 +0800

    update

--- Introduction/package_using.go
+++ Introduction/package_using.go
@@ -56,5 +56,53 @@ func main() {
 	//1.1.1包名。尽量和目录保持一致 尽量采用有意义的包名，简短，包名不要和标准库重名，包名采用全部小写
 	//1.1.2文件名。有多个单词可以采用蛇形命名法
 	//1.1.3变量名 蛇形python php 驼峰java c go
+	//1.1.4结构体命名 驼峰，User
+	//1.1.5接口命名 基本上和结构体差不多 推荐接口以er结尾
+	//1.1.6常量命名 全部大写，如果有多个单词使用蛇形 URL URLVersion bool类型Has Is can allow开头
+	//2.注释规范
+	//两种注视 //适合单行 /**/适合大段
+	//变量注释 包注释 接口注释 函数注释 代码逻辑的注释
+	//变量注释
+	// //1.
+	// c:=Person{
 
+	// }//2.
+
+	//结构体注释
+	//Course，对课程信息的包装
+	// type Course struct{
+	// 	Name string
+	// }
+
+	//package注释
+	//user 包，该包封装了一些结构体
+	// author bobby
+	// datetime：20221102
+
+	//函数注释
+	//GetCourse，获取课程的信息
+	//参数：
+	//。   c:Course对象
+	//返回值：
+	//课程的名称
+	// func GetCourse(c Course)string{
+	// 	return c.Name
+	// }
+
+	//3import注释
+	//3.1来源
+	//3.1.1go自带的包
+	//3.1.2第三方的包
+	//3.1.3自己内部的包
+	//分组+排序
+	// import(
+
+	// 	"os"
+	// 	"io"
+	// 	"encoding/json"
+
+	// 	"gorm.io/gorm"
+
+	// 	"src.imooc.com/bobby"
+	// )
 }
---
+++ Introduction/unit_test_using.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	//单元测试go test
+	//go test命令式一个按照一定约定和组织的测试代码驱动程序，在包中，所有以_test.go为后缀的源码文件都会被go test运行到
+	//我们写的_test.go源文件不会担心内容过多，因为go build不会讲这些测试文件打包到最后的可执行文件中
+	//test文件有四类
+	//1Test开头的功能测试 2Benchmark开头的性能测试 3example 4模糊测试
+}
