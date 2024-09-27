496a5eb1896e2e0b4242cb4c200eaf12d745c1b9
LZhenSheng <2759100807@qq.com>
2024-09-27 19:59:44 +0800

    update

--- Introduction/concurrency_using.go
+++ Introduction/concurrency_using.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 // python,java,php多线程编程、多进程编程，多线程和多进程存在的问题主要是耗费内存
@@ -33,6 +34,43 @@ func sub1() {
 		// total -= 1
 		// lock.Unlock()
 		atomic.AddInt32(&total, -1)
+	}
+}
+func producer(out chan<- int) {
+	for i := 0; i < 10; i++ {
+		out <- i * i
+	}
+	close(out)
+}
+func consumer(in <-chan int) {
+	for num := range in {
+		fmt.Println(num)
+	}
+}
+
+var number, letter = make(chan bool), make(chan bool)
+
+func printNum() {
+	i := 1
+	for {
+		<-number
+		//怎么做到，等待另一个goroutine来通知我
+		fmt.Printf("%d%d", i, i+1)
+		i += 2
+		letter <- true
+	}
+}
+func printLetter() {
+	i := 0
+	for {
+		<-letter
+		//怎么做到，等待另一个goroutine来通知我
+		if i > 25 {
+			return
+		}
+		fmt.Printf("%c%c", 'A'+i, 'A'+i+1)
+		i += 2
+		number <- true
 	}
 }
 func main() {
@@ -106,11 +144,123 @@ func main() {
 	//锁-竞争
 	//lock-灵活 atomic包-性能高
 	//锁复制后就失去了锁的效果
+	//锁本质是将并行的东西串形化了，使用lock肯定会影响性能
+	//即使是设计锁，那么也应该尽量的保证并行
+	// wg.Add(2)
+	// go add1()
+	// go sub1()
+	// wg.Wait()
+	// fmt.Println(total)
 
-	wg.Add(2)
-	go add1()
-	go sub1()
-	wg.Wait()
-	fmt.Println(total)
+	//我们有两组协程，其中一组负责写数据，另一组负责读数据，web系统绝大部分的场景都是读多写少
+	//虽然有多个goroutine，读之间并发，读写需要串形，写之间串形
+	//读写锁
+	// var num int
+	// var rwlock sync.RWMutex
+	// var wg sync.WaitGroup
+	// wg.Add(6)
+	// //写的goroutine
+	// go func() {
+	// 	time.Sleep(time.Second)
+	// 	defer wg.Done()
+	// 	rwlock.Lock() //加写锁，写锁会防止读写锁获取
+	// 	defer rwlock.Unlock()
+	// 	num = 12
+	// 	fmt.Println("get write lock")
+	// 	time.Sleep(time.Second * 5)
+	// }()
+	// //读的goroutine
+	// for i := 0; i < 5; i++ {
+	// 	go func() {
+	// 		defer wg.Done()
+	// 		for {
+	// 			rwlock.RLock() //读锁
+	// 			time.Sleep(time.Millisecond * 500)
+	// 			fmt.Println(num)
+	// 			rwlock.RUnlock()
+	// 		}
+	// 	}()
+	// }
+	// wg.Wait()
 
+	//不要通过共享内存来通讯，而要通过通讯来实现内存共享
+	//php python java多线程编程的时候，两个goroutine之间通讯最常用的方式是一个全局也会提供消息队列的机制，python-queue java 消费者和生产者之间的关系
+	//channel再加上语法堂使用channel更简单
+	//默认值nil
+	//channel=0时阻塞
+	//有缓冲和无缓冲的channel
+	//0=无缓冲的channel，只有一种用法，！=0是有缓冲的channel
+	// var msg chan string
+	// msg = make(chan string, 0)
+	// go func(msg chan string) {
+	// 	data := <-msg
+	// 	fmt.Println(data)
+	// }(msg)
+	// msg <- "bobby"
+	// time.Sleep(time.Second)
+	//waitGroup如果少了done调用，容易出现dealock，无缓冲的channel也容易出现
+	//无缓冲的channel适用于通知，b第一时间知道a是否已经完成
+	//有缓冲的channel适用于生产者消费者之间的通讯
+	//channel适用场景：1消息传递消息过滤2信号广播3时间订阅和广播4任务分发5结果汇总6并发控制7同步和异步
+
+	// var msg chan int
+	// msg = make(chan int, 2)
+	// go func(msg chan int) {
+	// 	for data := range msg {
+	// 		fmt.Println(data)
+	// 	}
+	// }(msg)
+	// msg <- 1
+	// msg <- 2
+	// close(msg) //与其他编程语言有很大区别
+	// msg<-3//已经关闭的channel不能放值，但可以取值
+	// time.Sleep(time.Second)
+
+	//----------------------------------
+	//单向channel
+	// var ch1 chan int
+	// var ch2 chan<- float64 //单向channel，只能写入
+	// var ch3 <-chan int     //单向cahnnel，只能读取
+
+	// c := make(chan int, 3)
+	// var send chan<- int = c //send-only
+	// var read <-chan int = c //recv-only
+	// send <- 1
+	// <-read
+
+	// c := make(chan int)
+	// go producer(c)
+	// go consumer(c)
+	// time.Sleep(10 * time.Second)
+
+	//------------------------------------------
+	//交替打印channel
+	//12AB34CD56EF78GH910IJ1112KL1314MN1516OP1718QR1920ST2122UV2324WX2526YZ2728%
+	// go printNum()
+	// go printLetter()
+	// number <- true
+	// time.Sleep(time.Second * 10)
+
+	//----------------------------------------
+	//select与switch相似，但是select的功能与linux的select、poll、epoll
+	//select主要作用于多个channel
+	//两个goroutine，某一个执行完成后，我们立马知道
+	// var lock sync.Mutex
+	g1 := make(chan struct{}) //channel多线程安全
+	g2 := make(chan struct{})
+	go func(c chan struct{}) {
+		time.Sleep(time.Second)
+		g1 <- struct{}{}
+	}(g1)
+	go func(c chan struct{}) {
+		time.Sleep(time.Second * 2)
+		g2 <- struct{}{}
+	}(g2)
+	select {
+	case <-g1:
+		fmt.Println("g1 done")
+	case <-g2:
+		fmt.Println("g2 done")
+	}
+	fmt.Println("done")
 }
