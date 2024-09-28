18cf5f348a2f6e5156d906d54339aaadba001a8c
LZhenSheng <2759100807@qq.com>
2024-09-28 22:21:23 +0800

    updte

---
+++ Introduction/Rpc/go_path_go_module.go
@@ -0,0 +1,17 @@
+package main
+
+// func main() {
+// 	//GO Path
+// 	//1.一定将代码新建到go path目录下的src
+// 	//2.要记得设置GO111MODULE=off
+// 	//先查找gopath/src这个目录下的包是否有goroot/src目录之下找
+// 	//其实不做包管理
+
+// 	//GO Module
+// 	//设置GO111MODULE=on
+
+// 	//go吐槽特点-包管理、异常处理、泛型
+
+// 	//总结能用go module就用go module，不用考虑以前的开发模式
+// 	//即使你是用了以前的模式，也可以自动设置为现在的modules模式
+// }
