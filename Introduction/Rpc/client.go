fa02f6a4c417c1f21ccf6fc4afe36ad8b9d0d70e
LZhenSheng <2759100807@qq.com>
2024-09-29 12:28:15 +0800

    rpc example

---
+++ Introduction/Rpc/client.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/kirinlabs/HttpRequest"
+)
+
+type ResponseData struct {
+	Data int `json:"data"`
+}
+
+func Add(a, b int) int {
+	req := HttpRequest.NewRequest()
+	req.SetTimeout(10 * time.Second)
+	res, _ := req.Get(fmt.Sprintf("http://127.0.0.1:8000/%s?a=%d&b=%d", "add", a, b))
+	body, _ := res.Body()
+	rspData := ResponseData{}
+	_ = json.Unmarshal(body, &rspData)
+	return rspData.Data
+}
+func main() {
+	fmt.Println(Add(2, 2))
+}
--- Introduction/Rpc/rpc_using.go
+++ Introduction/Rpc/rpc_using.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+)
 
 func add(a, b int) int {
 	return a + b
@@ -19,30 +25,30 @@ type PrintResult struct {
 	Err  error
 }
 
-func RpcPrintln(employee Employee) PrintResult {
-	/*
-		客户端
-		1.建立连接tcp/http
-		2.将employee对象序列化成json字符串-序列化
-		3.发送json字符串-调用成功后实际上你接受到的是一个二进制的数据
-		4.等待服务器发送结果
-		5将服务返回的数据解析成PrintResult对象-反序列化
-		服务端
-		1.监听网络接口80
-		2.读取数据-二进制的json数据
-		3.对数据进行反序列化Employee对象
-		4.开始处理业务逻辑
-		5.将处理的结果PrintReuslt发序列化成json二进制数据-序列化
-		6.将数据返回
-		序列化和反序列化是可以选择的，不一定要采用json、xml、protobuf、msgpack
-	*/
-}
+// func RpcPrintln(employee Employee) PrintResult {
+/*
+	客户端
+	1.建立连接tcp/http
+	2.将employee对象序列化成json字符串-序列化
+	3.发送json字符串-调用成功后实际上你接受到的是一个二进制的数据
+	4.等待服务器发送结果
+	5将服务返回的数据解析成PrintResult对象-反序列化
+	服务端
+	1.监听网络接口80
+	2.读取数据-二进制的json数据
+	3.对数据进行反序列化Employee对象
+	4.开始处理业务逻辑
+	5.将处理的结果PrintReuslt发序列化成json二进制数据-序列化
+	6.将数据返回
+	序列化和反序列化是可以选择的，不一定要采用json、xml、protobuf、msgpack
+*/
+// }
 func main() {
-	fmt.Println(add(1, 3))
+	// fmt.Println(add(1, 3))
 	//1.这个函数的调用参数如何传递-json（json是一种数据格式的协议）/xml/protobuf/msgpack-编码协议，json不是一个高性能的编码协议
 	//现在网络调用有两个端-客户端、应该干嘛？将数据传输到gin
 	//gin-服务端，服务端负责解析数据
-	fmt.Println(add(1, 2))
+	// fmt.Println(add(1, 2))
 	//将这个打印的工作放在另一个台服务器上，我需要将本地的内存对象struct，这样不行，可行的方式就是将struct序列化成json
 	// fmt.Println(Employee{
 	// 	Name: "bobby",
@@ -53,4 +59,29 @@ func main() {
 	// })
 	//远程服务器需要将二进制对象反解成struct对象
 	//搞这么麻烦，直接全部使用json去格式化不香嘛？这种做法在浏览器和gin服务之间是可以行，但是如果是一个大型的分布式系统不行
+
+	//get方法http://127.0.0.1:8000/add?a=1&b=2或http://127.0.0.1:8000?method=add&a=1&b=2
+	//返回的格式化：json{"data":3}
+	//1、callId的问题：r.URL.Path，2、数据的传输协议：url的参数传输协议，3、网络传输协议：http
+	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm() //解析参数
+		if err != nil {
+			panic("error")
+		}
+		fmt.Println("path:", r.URL.Path)
+		a, err := strconv.Atoi(r.Form["a"][0])
+		if err != nil {
+			panic("transform error")
+		}
+		b, err := strconv.Atoi(r.Form["b"][0])
+		if err != nil {
+			panic("transform error")
+		}
+		w.Header().Set("Content-Type", "application/json")
+		jData, err := json.Marshal(map[string]int{
+			"data": a + b,
+		})
+		w.Write(jData)
+	})
+	_ = http.ListenAndServe(":8000", http.TimeoutHandler(http.DefaultServeMux, time.Duration(10*time.Second), "Request timed out"))
 }
